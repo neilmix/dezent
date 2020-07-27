7cd8d4ee8c903b48d362d516bfedfce24e5a199c
Neil Mix <nmix@macbook-pro.lan>
2020-07-26 23:56:25 -0500

    Initial checkin, WIP.

---
+++ src/Grammar.ts
@@ -0,0 +1,105 @@
+export type Grammar = (DefineNode|ReturnNode)[];
+
+export interface Node { type: string }
+export interface SelectorNode extends Node { options: OptionNode[] }
+export interface RepeaterNode extends SelectorNode { repeat: RepeatString }
+export interface MatcherNode extends Node { match?(s : string) : number; }
+export type RepeatString = "*" | "+" | "?" | null;
+
+export type PartNode = CaptureNode | GroupNode | StringNode | RegexNode | RuleRefNode;
+export type ValueNode = BackRefNode | SplatNode | ObjectNode | ArrayNode | StringNode | NumberNode | BooleanNode | NullNode;
+
+export type ContextNode = ReturnNode | DefineNode | RuleNode | CaptureNode | GroupNode | OptionNode;
+
+export interface ReturnNode       extends Node         { type: "return",    rule: RuleNode }
+export interface DefineNode       extends Node         { type: "define",    name: string, rules: RuleNode[] }
+export interface RuleNode         extends SelectorNode { type: "rule",      output: ValueNode }
+export interface CaptureNode      extends RepeaterNode { type: "capture",   id?: number }
+export interface GroupNode        extends RepeaterNode { type: "group" }
+export interface OptionNode       extends Node         { type: "option",    tokens: PartNode[] }
+export interface RuleRefNode      extends Node         { type: "ruleref",   name: string }
+export interface RegexNode        extends MatcherNode  { type: "regex",     pattern: string }
+
+export interface StringNode       extends MatcherNode  { type: "string",    tokens: (StringEscapeNode|StringTextNode)[] }
+export interface StringTextNode   extends Node         { type: "text",      value: string }
+export interface StringEscapeNode extends Node         { type: "escape",    value: string }
+                                          
+export interface BackRefNode      extends Node { type: "backref",   id: string }
+export interface SplatNode        extends Node { type: "splat",     backrefs: ValueNode[] }
+export interface ObjectNode       extends Node { type: "object",    members: Member[] }
+export interface ArrayNode        extends Node { type: "array",     elements: ValueNode[] }
+export interface NumberNode       extends Node { type: "number",    value: string }
+export interface BooleanNode      extends Node { type: "boolean",   value: boolean }
+export interface NullNode         extends Node { type: "null" }
+
+export interface Member { name: BackRefNode|StringNode, value: ValueNode }
+
+let kNull:NullNode = { type: "null" }
+
+//let _ = $("_");
+
+export var dezentGrammar : Grammar = [
+    ret([], "4")
+];
+
+/*
+let grammar : (defineStT|returnStT)[] = [
+    ret([_, $_("any", $$(null, "defineSt","returnSt"), _)], 
+        "$1"),
+
+    def("_", ["/s*\\/"], kNull,
+        ["/\\/\\/[^\\n]*\\n/"], kNull,
+        "/\\/\\*(.|\\n)*\\*\\//", kNull),
+
+    def("returnSt", ["return", _, $("parse"), _, ";"],
+        { type: "return", parse: "$1" }),
+    
+    def("defineSt", [$("identifier"), _, "=", _, $("parse"), $_("any", _, ",", _, $("parse")), _, ";"],
+        { type: "define", name: "$1", parses: ["$2", splat("$3")]}),
+    
+    def("parse", [$("template"), _, "->", _, $("value")],
+        { template: "$1", value: "$2" }),
+
+    def("template", [$("templateOption"), $_("any", _, "|", _, $("templateOption"))],
+        { type: "template", options: ["$1", splat("$2")]}),
+    
+    def("templateOption", [$$("some", "templateCapture", "templateGroup", "string", "regex", "ruleref")],
+        { type: "option", parse: [splat("$1"), splat("$2, $3")]}),
+    
+    
+];*/
+
+// returnSt
+function ret(matcher: any, output: any) : ReturnNode {
+    return {
+        type: "return",
+        rule: {
+            type: "rule",
+            options: [{type: "option", tokens: matcher}],
+            output: { type: "boolean", value: true }
+        }
+    }
+}
+
+/*
+// templateGroup
+function $_(repeat:string, ...options) {
+}
+
+// templateCapture
+function $$(repeat:string, ...options) {
+}
+
+// shortcut templateCapture of one reference
+function $(id:string) {
+    return $$(null, id);
+}
+
+// defineSt
+function def(name:string, tpl1:any, val1:any, tpl2?:any, val2?:any, tpl3?:any, val3?:any) : defineStT {
+}
+
+function splat(...arg) : splatT {
+
+}
+*/
---
+++ src/Parser.ts
@@ -0,0 +1,387 @@
+import { 
+    Grammar, dezentGrammar, DefineNode, ReturnNode, 
+    ContextNode, RuleNode, OptionNode, RepeaterNode, CaptureNode, PartNode, 
+    ValueNode, BackRefNode, SplatNode, ObjectNode, ArrayNode, StringNode, StringTextNode, StringEscapeNode, NumberNode, BooleanNode, NullNode
+ } from "./Grammar";
+
+export enum ErrorCode {
+    DuplicateDefine = 1001,
+    MultipleReturn = 1002,
+    RuleNotFound = 1003,
+    ArrayOverrun = 2001,
+    MismatchOutputFrames = 2002,
+    CaptureAlreadyInProgress = 2003,
+    MismatchEndCapture = 2004,
+    EmptyOutput = 2005,
+}
+
+const errorMessages = {
+    1001: "Multiple rules defined with the same name: $1",
+    1002: "Grammars are only allowed to have one return statement",
+    1003: "Grammar does not contain a rule named '$1'",
+    2001: "Array overrun",
+    2002: "Mismatched output frames",
+    2003: "Capture already in progress",
+    2004: "Mismatched ending capture",
+    2005: "Output frame did not contain an output token",
+}
+
+export function parseText(grammarText:string, parseText:string) : any {
+    return parseTextWithGrammar(parseGrammar(grammarText), parseText);
+}
+
+export function parseGrammar(text:string) : Grammar {
+    return parseTextWithGrammar(dezentGrammar, text);
+}
+
+export function parseTextWithGrammar(grammar:Grammar, text:string) : any {
+    // pre-process the grammar
+    let defines: {[key:string]:DefineNode} = {};
+    let ret:ReturnNode;
+
+    for (let statement of grammar) {
+        switch (statement.type) {
+            case "define":
+                if (defines[statement.name]) {
+                    grammarError(ErrorCode.DuplicateDefine, statement.name);
+                }
+                defines[statement.name] = statement;
+                break;
+            case "return":
+                if (ret) grammarError(ErrorCode.MultipleReturn);
+                ret = statement;
+                break;
+        }
+    }
+
+    // now parse
+    let parser = new Parser(ret, text, defines);
+    parser.parse();
+
+    let builders = {
+        backref: null,
+        splat: null,
+        object: null,
+        array: null,
+        string: (node:StringNode) => {
+            return buildString(node);
+        },
+        number: (node:NumberNode) => {
+            return Number(node.value);
+        },
+        boolean: (node:BooleanNode) => {
+            return node.value;
+        },
+        null: () => {
+            return null;
+        },
+    };
+
+    // build our output value
+    return buildOutput(parser.output.result);
+
+    function buildOutput(token:OutputToken) {
+        if (token.backrefs && token.value) {
+            let backrefs = token.backrefs.map((v) => buildOutput(v));
+            return builders[token.value.type](token.value, backrefs);
+        } else {
+            return text.substr(token.pos, token.length);
+        }
+    }
+}
+
+type ParseContextFrame = {
+    node: ContextNode,
+    items: RuleNode[] | OptionNode[] | PartNode[],
+    index: number,
+    startPos: number,
+    pos: number,
+    matchCount: number,
+}
+
+type OutputContextFrame = {
+    node : ReturnNode|DefineNode,
+    captureMap : number[],
+    captureNode : CaptureNode|null,
+    captures : OutputToken[][],
+    output?: OutputToken
+}
+
+interface OutputToken {
+    pos: number,
+    length: number
+    backrefs?: OutputToken[],
+    value?: ValueNode,
+}
+
+class OutputContext {
+    stack:OutputContextFrame[] = [];
+    top:OutputContextFrame = null;
+    captureIdSequence = 0;
+    result:OutputToken;
+
+    constructor() {
+    }
+
+    enter(node:ReturnNode|DefineNode) {
+        this.top = {
+            node: node,
+            captureMap: [],
+            captureNode: null,
+            captures: [],
+        };
+        this.stack.push(this.top);
+    }
+
+    exit(node:ReturnNode|DefineNode, success:boolean) {
+        let frame = this.stack.pop();
+        if (frame.node != node) {
+            parserError(ErrorCode.MismatchOutputFrames);
+        }
+        this.top = this.stack[this.stack.length-1]
+        if (!frame.output) {
+            parserError(ErrorCode.EmptyOutput);
+        }
+        this.addTokenObject(frame.output);
+    }
+
+    startCapture(node:CaptureNode) {
+        if (this.top.captureNode) {
+            parserError(ErrorCode.CaptureAlreadyInProgress);
+        }
+        this.top.captureNode = node;
+        if (!node.id) {
+            node.id = ++this.captureIdSequence;
+        }
+        if (!this.top.captureMap[node.id]) {
+            this.top.captureMap[node.id] = this.top.captures.length;
+            this.top.captures.push([]);
+        }
+    }
+
+    endCapture(node:CaptureNode) {
+        if (this.top.captureNode != node) {
+            parserError(ErrorCode.MismatchEndCapture);
+        }
+        this.top.captureNode = null;
+    }
+
+    addToken(pos, consumed) {
+        this.addTokenObject({
+            pos: pos,
+            length: consumed
+        })
+    }
+
+    addTokenObject(token:OutputToken) {
+        if (this.top) {
+            this.top.captures[this.top.captureMap[this.top.captureNode.id]].push(token);
+        } else {
+            this.result = token;
+        }
+    }
+
+    yield(node:RuleNode, startPos:number, consumed:number) {
+        let backrefs : OutputToken[] = [];
+        for (let capture of this.top.captures) {
+            if (capture.length > 1) {
+                backrefs.push({
+                    pos: capture[0].pos,
+                    length: capture.reduce((t, c) => t + c.length, 0)
+                })
+            } else {
+                backrefs.push(capture[0]);
+            }
+        }
+        this.top.output = {
+            pos: startPos,
+            length: consumed,
+            backrefs: backrefs,
+            value: node.output
+        }
+    }
+
+    reset() {
+        this.top.captureMap = [];
+        this.top.captures = [];
+    }
+}
+
+class Parser {
+    stack : ParseContextFrame[] = [];
+    text : string;
+    defines: {[key:string]:DefineNode};
+    output : OutputContext = new OutputContext();
+
+    get top():ParseContextFrame|null { return this.stack[this.stack.length-1] }
+
+    constructor(root:ReturnNode, text:string, defines:{[key:string]:DefineNode}) {
+        this.text = text;
+        this.defines = defines;
+        this.enter(root);
+    }
+
+    parse() {
+        while (true) {
+            // find the next part
+            let part;
+            PART: while (true) {
+                let current = this.top;
+                if (current.index >= current.items.length) {
+                    parserError(ErrorCode.ArrayOverrun);
+                } else {
+                    switch (current.node.type) {
+                        case "return":  case "define": case "rule": case "capture": case "group":
+                            this.enter((<RuleNode[]|OptionNode[]>current.items)[current.index]);
+                            break;
+                        case "option":
+                            part = (<PartNode[]>current.items)[current.index];
+                            break PART;
+                    }
+                }
+            }
+
+            // execute the part
+            switch(part.type) {
+                case "capture": case "group":
+                    this.enter(part);
+                    break;
+                case "ruleref":
+                    if (this.defines[part.name]) {
+                        this.enter(this.defines[part.name]);
+                    } else {
+                        grammarError(ErrorCode.RuleNotFound, part.name)
+                    }
+                    break;
+                case "string":
+                case "regex":
+                    if (!part.match) {
+                        if (part.type == "string") {
+                            let matchString = buildString(part);
+                            part.match = (s) => s.startsWith(matchString) ? matchString.length : -1;
+                        } else {
+                            let regex = new RegExp(`^${part.pattern}`)
+                            part.match = (s) => {
+                                let result = regex.exec(s);
+                                return result ? result[0].length : -1;
+                            }
+                        }
+                    }
+                    let consumed = part.match(this.text.substr(this.top.pos));
+                    if (consumed >= 0) {
+                        let current = this.top;
+                        this.output.addToken(current.pos, consumed);
+                        current.pos += consumed;
+                        if (current.index >= current.items.length - 1) {
+                            // everything passed, or else we would have popped
+                            this.exit(true);
+                        }
+                    } else {
+                        this.exit(false);
+                    }
+                    break;
+            }
+        } 
+        
+        // check that consumed == text.length...
+
+    }
+
+    enter(node:ContextNode) {
+        let current = this.top;
+        let items;
+
+        switch (node.type) {
+            case "return": 
+                items = [node.rule]; 
+                this.output.enter(node);
+                break;
+            case "define": 
+                items = node.rules; 
+                this.output.enter(node);
+                break;
+            case "capture": 
+                this.output.startCapture(node);
+                // FALL THROUGH
+            case "group": 
+            case "rule": items = node.options; break;
+            case "option": items = node.tokens; break;
+        }
+        this.stack.push({
+            node: node,
+            items: items,
+            index: 0,
+            startPos: current ? current.pos : 0,
+            pos: current ? current.pos : 0,
+            matchCount: 0,
+        })
+    }
+
+    exit(success:boolean) {
+        while (this.stack.length) {
+            let exited = this.stack.pop();
+            switch (exited.node.type) {
+                case "return":
+                case "define":
+                    this.output.exit(exited.node, success);
+                    break;
+                case "rule":
+                    if (success) {
+                        this.output.yield(exited.node, exited.startPos, exited.pos - exited.startPos);
+                    } else {
+                        this.output.reset();
+                    }
+                    break;
+                case "capture":
+                    this.output.endCapture(exited.node);
+                    // FALL THROUGH
+                case "group":
+                    let required = exited.node.repeat in [null,'+'];
+                    if (!required) success = true;
+                    if (exited.node.repeat == '*') success = true;
+                    if (exited.node.repeat == '+' && exited.matchCount > 0) success = true;
+                    break;
+                case "option":
+                    // do nothing - we'll mitigate the parent below
+                    break;
+            }
+
+            let current = this.top;
+            if (success) {
+                if (current.node.type in ["group", "capture"] && (<RepeaterNode>current.node).repeat in ["*","+"]) {
+                    current.index = 0;
+                    current.matchCount++;
+                    return;
+                }
+                current.index++;
+                if (current.index < current.items.length) {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function buildString(node:StringNode) {
+    return node.tokens.map((node:StringTextNode|StringEscapeNode) => {
+        if (node.type == "text") {
+            return node.value;
+        } else if (node.value[0] == 'u') {
+            return String.fromCharCode(Number(`0x${node.value.substr(1)}`));
+        } else if ("bfnrt".indexOf(node.value[1]) >= 0) {
+            return ({ b:'\b', f:'\f', n:'\n', r:'\r', t:'\t' })[node.value[1]];
+        } else {
+            return node.value.substr(1);
+        }
+    }).join("")
+}
+
+function grammarError(code:ErrorCode, arg1?:string) {
+    let msg = errorMessages[code].replace("$1", arg1);
+    throw new Error(`Grammar error: ${msg}`);
+}    
+
+function parserError(code:ErrorCode) {
+    let msg = errorMessages[code];
+    throw new Error(`Internal parser error: ${msg}`);
+}
